3de06a6df37fa63e7ba5a0e706d53436c36cd0d6
monsolos <143030773+monsolos@users.noreply.github.com>
2023-10-19 13:25:17 -0400

    Update HelloTest1.java
    I added the date of the update to line 3 and changed the comments on line 9.

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,12 @@
 /*
  * Created on 8/3/18 by AJT
+ * Update on 10/19/2023 SS
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first pull request.
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
